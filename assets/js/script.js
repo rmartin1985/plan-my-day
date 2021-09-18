b7b369f4e9378b57a3581cfe936d6658862d3190
RayceWheat <RayceWheat@gmail.com>
2021-09-18 15:20:24 -0500

    removed rating 7 from being dispalyed

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,30 +52,37 @@ var displayMuseums = function() {
     apiCall = 'https://api.opentripmap.com/0.1/en/places/radius?radius=40233&lon=' 
     + lon 
     + '&lat=' + lat + 
-    '&kinds=museums&rate=3&format=json&limit=5&apikey=5ae2e3f221c38a28845f05b6d3c5612fbf8d9a0bb1b85e69d409790b';
+    '&kinds=museums&rate=3&format=json&limit=10&apikey=5ae2e3f221c38a28845f05b6d3c5612fbf8d9a0bb1b85e69d409790b';
     
     // turning the api call into json
     fetch(apiCall)
     .then(function(response){
         return response.json();
     })
-    .then(function(response){
+    .then(function(response) {
         //printing out to the page the names things
         console.log(response);
         for (var i = 0; i < response.length; i++) {
 
+            // Skipping anything with a rating of 7
+            if (response[i].rate === 7) {
+
+                console.log('a 7 was skipped');
+
+            } else {
+            // Selecting the mmuseums div
             var infoEl = document.getElementById('museums-div');
-    
+            // adding to the div an <h2> tag with the name of the place
             infoEl.innerHTML += '<h2>' + response[i].name + '</h2>';
-        }
-    })
-}
+          }
+         }
+       })
 
 displayRestaurants = function() {
     apiCall = 'https://api.opentripmap.com/0.1/en/places/radius?radius=40233&lon=' 
     + lon 
     + '&lat=' + lat + 
-    '&kinds=restaurants&rate=3&format=json&limit=5&apikey=5ae2e3f221c38a28845f05b6d3c5612fbf8d9a0bb1b85e69d409790b';
+    '&kinds=restaurants&rate=3&format=json&limit=10&apikey=5ae2e3f221c38a28845f05b6d3c5612fbf8d9a0bb1b85e69d409790b';
     
     // turning the api call into json
     fetch(apiCall)
@@ -87,9 +94,19 @@ displayRestaurants = function() {
         console.log(response);
         for (var i = 0; i < response.length; i++) {
 
+            // Skipping anything with a rating of 7
+            if (response[i].rate === 7) {
+
+                console.log('a 7 was skipped');
+
+            } else {
+            
+            // Selecting the restaurants div
             var infoEl = document.getElementById('restaurants-div');
-    
+            
+            // adding a <h2> with the name 
             infoEl.innerHTML += '<h2>' + response[i].name + '</h2>';
+          }
         }
     })
 }
@@ -98,7 +115,7 @@ displayBars = function() {
     apiCall = 'https://api.opentripmap.com/0.1/en/places/radius?radius=40233&lon=' 
     + lon 
     + '&lat=' + lat + 
-    '&kinds=bars&rate=3&format=json&limit=5&apikey=5ae2e3f221c38a28845f05b6d3c5612fbf8d9a0bb1b85e69d409790b';
+    '&kinds=bars&rate=3&format=json&limit=10&apikey=5ae2e3f221c38a28845f05b6d3c5612fbf8d9a0bb1b85e69d409790b';
     
     // turning the api call into json
     fetch(apiCall)
@@ -110,9 +127,18 @@ displayBars = function() {
         console.log(response);
         for (var i = 0; i < response.length; i++) {
 
+            // Skipping anything with a rating of 7
+            if (response[i].rate === 7) {
+
+            console.log('a 7 was skipped');
+
+            } else {
+            // Selecting the bars div
             var infoEl = document.getElementById('bars-div');
-    
+            
+            // Adding an <h2> tag to the div 
             infoEl.innerHTML += '<h2>' + response[i].name + '</h2>';
+            }
         }
     })
 }
@@ -121,7 +147,7 @@ displayMonuments = function() {
     apiCall = 'https://api.opentripmap.com/0.1/en/places/radius?radius=40233&lon=' 
     + lon 
     + '&lat=' + lat + 
-    '&kinds=monuments&rate=3&format=json&limit=5&apikey=5ae2e3f221c38a28845f05b6d3c5612fbf8d9a0bb1b85e69d409790b';
+    '&kinds=monuments&rate=3&format=json&limit=10&apikey=5ae2e3f221c38a28845f05b6d3c5612fbf8d9a0bb1b85e69d409790b';
     
     // turning the api call into json
     fetch(apiCall)
@@ -133,9 +159,16 @@ displayMonuments = function() {
         console.log(response);
         for (var i = 0; i < response.length; i++) {
 
+            // Skipping anything with a rating of 7
+            if (response[i].rate === 7) {
+
+                console.log('a 7 was skipped');
+            } else {
+            // Selecting the monuments div
             var infoEl = document.getElementById('monuments-div');
-    
+            // adding a <h2> tag to the div and adding the name 
             infoEl.innerHTML += '<h2>' + response[i].name + '</h2>';
+            }
         }
     })
-}
+}}
