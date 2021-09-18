146979469530f90a2a56807392f4be7ef9e46a1e
Jwbranch4 <87912530+Jwbranch4@users.noreply.github.com>
2021-09-18 09:48:32 -0500

    added jquery link and added function call for ticketmaster api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,75 @@
-// Selecting the user's text field 
-var textEl = document.getElementById('city');
-// Selecting the button element 
-var buttonEl = document.getElementById('userInput');
+// Selecting the user's text field
+var textEl = document.getElementById("city");
+// Selecting the button element
+var buttonEl = document.getElementById("userInput");
 
-// Adding an event listenr to the button upon click which will display info based on the user input 
+// Adding an event listenr to the button upon click which will display info based on the user input
 buttonEl.addEventListener("click", getUserInput);
 
-// Function that will be used to display places on the page 
-// takes as argument lat and lon 
-var displayPlaces = function(lat, lon) {
+// Function that will be used to display places on the page
+// takes as argument lat and lon
+var displayPlaces = function (lat, lon) {
+  // Making an API call based on Lat and Lon from the user's City
+  // The API Call displays the top 25 things in 25 miles from the lat,lon
+  // API Call can filter based on popularity I have choosen only the most popular
+  apiCall =
+    "https://api.opentripmap.com/0.1/en/places/radius?radius=40233&lon=" +
+    lon +
+    "&lat=" +
+    lat +
+    "&rate=3&format=json&limit=25&apikey=5ae2e3f221c38a28845f05b6d3c5612fbf8d9a0bb1b85e69d409790b";
 
-    // Making an API call based on Lat and Lon from the user's City
-    // The API Call displays the top 25 things in 25 miles from the lat,lon
-    // API Call can filter based on popularity I have choosen only the most popular
-    apiCall = 'https://api.opentripmap.com/0.1/en/places/radius?radius=40233&lon=' + lon + '&lat=' + lat + '&rate=3&format=json&limit=25&apikey=5ae2e3f221c38a28845f05b6d3c5612fbf8d9a0bb1b85e69d409790b';
-
-    // turning the api call into json
-    fetch(apiCall)
-    .then(function(response){
-        return response.json();
-        
+  // turning the api call into json
+  fetch(apiCall)
+    .then(function (response) {
+      return response.json();
     })
-    .then(function(response){
-        //printing out to the page the names things
-        console.log(response);
-        for (var i = 0; i < response.length; i++) {
+    .then(function (response) {
+      //printing out to the page the names things
+      console.log(response);
+      for (var i = 0; i < response.length; i++) {
+        var infoEl = document.getElementById("info");
 
-            var infoEl = document.getElementById('info');
-    
-            infoEl.innerHTML += '<h2>' + response[i].name + '</h2>';
-        }
-    })
-}
+        infoEl.innerHTML += "<h2>" + response[i].name + "</h2>";
+      }
+    });
+};
 
-// Function called when the button is clicked 
+// Function called when the button is clicked
 function getUserInput() {
-    // Making an api call using the user input as the city name 
-    apiCall = ('https://api.opentripmap.com/0.1/en/places/geoname?name=' + textEl.value +'&apikey=5ae2e3f221c38a28845f05b6d3c5612fbf8d9a0bb1b85e69d409790b');
+  // Making an api call using the user input as the city name
+  apiCall =
+    "https://api.opentripmap.com/0.1/en/places/geoname?name=" +
+    textEl.value +
+    "&apikey=5ae2e3f221c38a28845f05b6d3c5612fbf8d9a0bb1b85e69d409790b";
 
-    // turning the API call into JSON
-    fetch(apiCall)
-    .then(function(response){
-        return response.json();
+  // turning the API call into JSON
+  fetch(apiCall)
+    .then(function (response) {
+      return response.json();
     })
-    .then(function(response){
+    .then(function (response) {
+      //Pulling the lat and lon values to be used in another API call
+      lat = response.lat;
+      lon = response.lon;
 
-        //Pulling the lat and lon values to be used in another API call
-        lat = response.lat;
-        lon = response.lon; 
-        
-        //Calling the function to display places based on the lat and lon of the city name
-        displayPlaces(lat, lon);
-    })
+      //Calling the function to display places based on the lat and lon of the city name
+      displayPlaces(lat, lon);
+    });
 }
+
+// Function pull ticketmaster info
+$.ajax({
+  type: "GET",
+  url: "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&size=1&apikey=iHQWV72eUoMRF8CqNt6SxnF49uNdDeK8",
+  async: true,
+  dataType: "json",
+  success: function (json) {
+    console.log(json);
+    // Parse the response.
+    // Do other things.
+  },
+  error: function (xhr, status, err) {
+    // This time, we do not end up here!
+  },
+});
